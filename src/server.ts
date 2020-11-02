79f39e146c4eb3a0da2b49472adce579c3e43c96
Diogo <diogo.mendes@squadra.com.br>
2020-11-02 19:46:45 -0300

    Codigo

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+// src/server.ts
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log('Servidor subiu !! ');
+});
